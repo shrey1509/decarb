44a898fcdec03e3fb21c4e4fff6468ad21946f27
ShreyasDorle <shreyasdorle.it@gmail.com>
2021-07-12 23:43:23 +0530

    did smol QoL changes

--- animations/enquiry.js
+++ animations/enquiry.js
@@ -44,7 +44,7 @@ function myFunction(x) {
   if (x.matches) {
        document.getElementById("content-title").classList.remove("offset-md-4","col-md-6");
        document.getElementById("content-field-1").classList.remove("offset-md-4","col-md-3","offset-md-1");
-       document.getElementById("content-field-2").classList.remove("offset-md-4","col-md-2");
+       document.getElementById("content-field-2").classList.remove("offset-md-4","col-md-3");
        document.getElementById("content-field-3").classList.remove("col-md-3");
        document.getElementById("content-field-4").classList.remove("offset-md-4","col-md-3");
        document.getElementById("content-field-5").classList.remove("col-md-2");
@@ -52,7 +52,7 @@ function myFunction(x) {
   } else {
        document.getElementById("content-title").classList.add("offset-md-4","col-md-6");
        document.getElementById("content-field-1").classList.add("offset-md-4","col-md-3","offset-md-1");
-       document.getElementById("content-field-2").classList.add("offset-md-4","col-md-2");
+       document.getElementById("content-field-2").classList.add("offset-md-4","col-md-3");
        document.getElementById("content-field-3").classList.add("col-md-3");
        document.getElementById("content-field-4").classList.add("offset-md-4","col-md-3");
        document.getElementById("content-field-5").classList.add("col-md-2");
@@ -62,3 +62,58 @@ function myFunction(x) {
 var x = window.matchMedia("(min-width:768px) and (max-width: 1024px)") ;
 myFunction(x); 
 x.addListener(myFunction) ;
+
+function validate(name, phone, email, car){
+       var phoneno = /^\d{10}$/;
+       var numberOnly = /^\d+$/;
+       var flag = 0;
+       if(name === "" && name.length == 0) {
+            flag = 0;
+            $("#name").addClass("is-invalid");
+            return flag;
+       } else {
+            flag = 1;
+            $("#name").removeClass("is-invalid");
+       }
+
+       if(phone === "" || !phone.match(phoneno)) {
+              flag = 0;
+              $("#phone").addClass("is-invalid");
+              return flag;
+       } else {
+              flag = 1;
+              $("#phone").removeClass("is-invalid");
+       }  
+
+       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && email.endsWith(".com")){
+              flag = 1;
+              $("#email").removeClass("is-invalid");
+       } else {
+              flag = 0;
+              $("#email").addClass("is-invalid");
+              return flag;
+       }
+
+       if(car === "" && car.length == 0) {
+            flag = 0;
+            $("#car").addClass("is-invalid");
+            return flag;
+       } else {
+            flag = 1;
+            $("#car").removeClass("is-invalid");
+       }
+
+       return flag;   
+}
+
+function ValidateEmailAndSubmit() {
+       var name = document.getElementById('name').value;
+       var phone = document.getElementById('phone').value;
+       var email = document.getElementById('email').value;
+       var car = document.getElementById('car').value;
+       var form = document.getElementById('upload-form');
+       if(validate(name, phone, email, car)){
+              alert('Details Submitted, we will contact you soon.')
+              form.submit();
+        }
+    }
--- pages/enquiry.php
+++ pages/enquiry.php
@@ -17,21 +17,31 @@
 				<h4>Please fill in this form so we can contact you</h4>
 			</div>
 		</div>
-		<form action="admin/contactupload.php" method="POST">
+		<form action="admin/contactupload.php" method="POST" id="upload-form">
 			<div class="contact-fields row">
 				<div class="form-group" id="content-field-1">
 				  <label for="name">Full Name:</label>
 				  <input type="text" class="contact-input form-control" name="name" id="name">
+				  <div class="invalid-feedback">Enter Name</div>
 				</div>
 			</div>
 			<div class="contact-fields row">
 				<div class="form-group" id="content-field-2">
 				  <label for="phone">Phone Number:</label>
-				  <input type="text" class="contact-input form-control" name="phone" id="phone">
+				  <!-- <input type="text" class="contact-input form-control" name="phone" id="phone">
+				  <div class="invalid-feedback">Enter 10 digit phone number</div> -->
+				  	<div class="input-group">
+				      <div class="input-group-prepend">
+				          <span class="input-group-text">+91</span>
+				      </div>
+				      <input type="text" class="contact-input form-control" name="phone" id="phone" />
+				      <div class="invalid-feedback">Enter 10 digit phone number</div>
+				  	</div>
 				</div>
 				<div class="form-group" id="content-field-3">
 				  <label for="email">Email:</label>
 				  <input type="text" class="contact-input form-control" name="email" id="email">
+				  <div class="invalid-feedback">Enter valid email ID</div>
 				</div>
 
 			</div>
@@ -39,16 +49,24 @@
 				<div class="form-group" id="content-field-4">
 				  <label for="car">Car Name/Model:</label>
 				  <input type="text" class="contact-input form-control" name="car" id="car">
+				  <div class="invalid-feedback">Enter car name/model</div>
 				</div>
 				<div class="form-group" id="content-field-5">
 				  <label for="cc">Engine CC:</label>
-				  <input type="text" class="contact-input form-control" name="cc" id="cc">
+				  <!-- <input type="text" class="contact-input form-control" name="cc" id="cc"> -->
+				  <div class="input-group">
+				      <input type="text" class="contact-input form-control" name="cc" id="cc" />
+				      <div class="input-group-append">
+				          <span class="input-group-text">CC</span>
+				      </div>
+				      <!-- <div class="invalid-feedback">Enter 10 digit phone number</div> -->
+				  	</div>
 				</div>
 			
 			</div>
 			<div class="contact-fields row">
 				<div class=" submit-button" id="content-field-6">
-					<button type="submit" class="btn enquiry-link" name="submit" id="submit-link">Submit</button>
+					<button type="button" class="btn enquiry-link" onclick="ValidateEmailAndSubmit();" name="submit-form" id="submit-link">Submit</button>
 				</div>
 			</div>
 		</form>
